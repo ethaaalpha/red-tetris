662c75835c7aa31303b83f51b4a69fee3d69e787
loku74 <loicbn7@gmail.com>
2026-01-05 20:25:45 +0100

    empty line on chat event

--- server/src/events/chat.ts
+++ server/src/events/chat.ts
@@ -6,7 +6,6 @@ import type { Callback } from "../types/types";
 export function registerHandlers(socket: Socket) {
   socket.on("chat", (data: SocketChatData, callback: Callback) => {
     const result = validateChat(socket, data);
-
     if (!result.status) {
       callback(false, result.error);
       return;
