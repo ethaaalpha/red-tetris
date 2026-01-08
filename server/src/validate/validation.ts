8f4eb570804197805be274f4899f39d5487a18fb
Loic Bourniquel <lbourniq@z3r8p4.42lyon.fr>
2026-01-08 09:57:34 +0100

    rebase rc-1

--- server/src/validate/validation.ts
+++ server/src/validate/validation.ts
@@ -1,11 +1,23 @@
 import z, { ZodError } from "zod";
+
 import {
+  USERNAME_MAX_LENGTH,
+  ROOM_MAX_LENGTH,
   CHAT_MAX_LENGTH,
   REGEX_MESSAGE,
-  REGEX_ROOM_USER,
-  ROOM_MAX_LENGTH,
-  USERNAME_MAX_LENGTH
+  REGEX_ROOM_AND_USER
 } from "../constants/core";
+
+import {
+  Z_USERNAME_EMPTY,
+  Z_USERNAME_MAX,
+  Z_ROOM_EMPTY,
+  Z_ROOM_MAX,
+  Z_MESSAGE_EMPTY,
+  Z_MESSAGE_MAX,
+  Z_REGEX_ROOM_AND_USER,
+  Z_REGEX_MESSAGE_SANITIZE
+} from "../constants/error";
 
 export function formatSchemeError(error: ZodError) {
   return error.issues.reduce(
@@ -20,18 +32,18 @@ export function formatSchemeError(error: ZodError) {
 
 export const roomValidation = z
   .string()
-  .regex(REGEX_ROOM_USER, "Only alphanumeric characters and -_ are authorized")
-  .min(1, "Room name cannot be empty")
-  .max(ROOM_MAX_LENGTH, `Room name cannot be longer than ${ROOM_MAX_LENGTH} characters`);
+  .regex(REGEX_MESSAGE, Z_REGEX_MESSAGE_SANITIZE)
+  .min(1, Z_ROOM_EMPTY)
+  .max(ROOM_MAX_LENGTH, Z_ROOM_MAX);
 
 export const usernameValidation = z
   .string()
-  .regex(REGEX_ROOM_USER, "Only alphanumeric characters and -_ are authorized")
-  .min(1, "Username cannot be empty")
-  .max(USERNAME_MAX_LENGTH, `Username cannot be longer than ${USERNAME_MAX_LENGTH} characters`);
+  .regex(REGEX_ROOM_AND_USER, Z_REGEX_ROOM_AND_USER)
+  .min(1, Z_USERNAME_EMPTY)
+  .max(USERNAME_MAX_LENGTH, Z_USERNAME_MAX);
 
 export const messageValidation = z
   .string()
-  .regex(REGEX_MESSAGE)
-  .min(1, "Message cannot be empty")
-  .max(CHAT_MAX_LENGTH, `Message cannot be longer than ${CHAT_MAX_LENGTH} characters`);
+  .regex(REGEX_ROOM_AND_USER, Z_REGEX_ROOM_AND_USER)
+  .min(1, Z_MESSAGE_EMPTY)
+  .max(CHAT_MAX_LENGTH, Z_MESSAGE_MAX);
