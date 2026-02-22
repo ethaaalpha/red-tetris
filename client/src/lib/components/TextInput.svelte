2c0ba1543c8a83a29b8c1b3f80d46222a5aea3a8
Loïc <92410060+loku74@users.noreply.github.com>
2026-02-22 21:33:30 +0100

    fix: removed keyboard event when typing a message (#142)
    * rc-1
    
    * renamed to focused

--- client/src/lib/components/TextInput.svelte
+++ client/src/lib/components/TextInput.svelte
@@ -2,6 +2,7 @@
   let {
     value = $bindable(""),
     input = $bindable(),
+    focused = $bindable(false),
     maxlength,
     placeholder = "",
     error = undefined,
@@ -15,6 +16,7 @@
   }: {
     value: string;
     input?: HTMLInputElement;
+    focused?: boolean;
     maxlength: number;
     placeholder?: string;
     error?: string;
@@ -26,8 +28,6 @@
     bright?: boolean;
     regex?: RegExp;
   } = $props();
-
-  let isFocused = $state(false);
 
   function format(v: string) {
     if (regex !== undefined) {
@@ -48,8 +48,8 @@
       bind:this={input}
       bind:value
       oninput={() => (value = format(value))}
-      onfocus={() => (isFocused = true)}
-      onblur={() => (isFocused = false)}
+      onfocus={() => (focused = true)}
+      onblur={() => (focused = false)}
       class="w-full outline-red-primary py-2 pl-4 pr-18
       {bright ? 'bg-dark-accent' : ' bg-dark-secondary'}
       {outline ? `focus:outline-2` : 'outline-none'}
@@ -61,7 +61,7 @@
       type="text"
       {placeholder}
     />
-    {#if isFocused}
+    {#if focused}
       <span
         class="absolute right-4 top-1/2 -translate-y-1/2 text-sm {value.length >= maxlength
           ? 'text-red-400'
