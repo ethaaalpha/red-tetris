7e58e38a3f92c9fb352628050997a370e14d3b93
loku74 <loicbn7@gmail.com>
2026-03-04 16:03:10 +0100

    lol

--- server/src/validate/spectate.ts
+++ server/src/validate/spectate.ts
@@ -34,10 +34,13 @@ export function validateSpectate(
   payload: EventSpectatePayload
 ): ValidateSpectateResult {
   const result = schema.safeParse(payload);
+  console.log("lol0");
 
   if (!result.success) {
     return { status: false, error: formatSchemeError(result.error) };
   }
+
+  console.log("lol1");
 
   const current = getUser(socket.id);
   const room = getRoomBySocket(socket);
@@ -46,23 +49,29 @@ export function validateSpectate(
     return { status: false, error: { room: ERROR_NOT_IN_A_ROOM } };
   }
 
+  console.log("lol2");
   if (!room.game || !room.game.ongoing) {
     return { status: false, error: { user: ERROR_NOT_IN_GAME } };
   }
 
+  console.log("lol3");
   const currentPlayer = room.game.getPlayer(current.id);
   if (!currentPlayer.alive) {
     return { status: false, error: { user: ERROR_SELF_DEAD } };
   }
 
+  console.log("lol4");
   const userToSpectate = room.get(result.data.username);
   if (!userToSpectate) {
     return { status: false, error: { user: ERROR_USER_NOT_FOUND } };
   }
+
+  console.log("lol5");
   if (!room.userExists(userToSpectate)) {
     return { status: false, error: { room: ERROR_USER_NOT_FOUND } };
   }
 
+  console.log("lol6");
   const playerToSpectate = room.game.getPlayer(userToSpectate.id);
   if (!playerToSpectate.alive) {
     return { status: false, error: { user: ERROR_PLAYER_DEAD } };
